ab4ffbb3decb8b369074c48e4d53fd95c7df5fb8
Dat-die <128242145+Diey-na@users.noreply.github.com>
2024-10-17 20:46:53 +0200

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -38,7 +38,7 @@ def parameter(df_sp, sector_default_val, cap_default_val):
 
 @st.cache
 def read_data():
-    path_data = 'udemy_streamlit/initial_version/project/s&p500.csv'
+    path_data = 's&p500.csv'
     df_sp = pd.read_csv(path_data)
     return df_sp
 
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     st.title('S&P500 Screener & Stock Analysis')
     st.sidebar.title('Search criteria')
 
-    image_path = 'udemy_streamlit/initial_version/project/stock.jpeg'
+    image_path = 'stock.jpeg'
     try:
         image = Image.open(image_path)
         _, col_image_2, _ = st.columns([1, 3, 1])
